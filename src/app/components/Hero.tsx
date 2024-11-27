9866dcabdcacfe5b1b5272e771bcc8d61a5dc18d
Aathif <aathifzahir12@gmail.com>
2024-11-27 19:17:40 +0530

    Hero Section added

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,11 @@
+export default function Hero() {
+  return (
+    <div className="bg-background py-4">
+      <div className="container mx-auto space-y-4 p-20">
+        <h1 className="text-8xl font-bold text-white">Aathif Zahir</h1>
+        <h1 className="text-8xl font-bold text-white">Developer</h1>
+        <div className="text-2xl text-white">Based in Sri Lanka</div>
+      </div>
+    </div>
+  );
+}
