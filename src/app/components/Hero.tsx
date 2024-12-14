e1c39a0e87662ba3f6330e0bef74e97c4358ae69
Aathif <aathifzahir12@gmail.com>
2024-12-14 17:46:35 +0530

    mouse scroll animation added

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,10 +1,18 @@
+import React from "react";
+import styles from "../styles/mouse.module.css"; // Import the CSS module
+
 export default function Hero() {
   return (
-    <div className="bg-background py-4">
-      <div className="container mx-auto space-y-4 p-20">
-        <h1 className="text-8xl font-bold text-white">Aathif Zahir</h1>
-        <h1 className="text-8xl font-bold text-white">Developer</h1>
-        <div className="text-2xl text-white">Based in Sri Lanka</div>
+    <div className="bg-background py-4  items-center  min-h-screen">
+      <div className="container mx-auto space-y-4 p-20 text-center">
+        <h1 className="text-8xl font-bold text-white">AATHIF ZAHIR</h1>
+        <h1 className="text-4xl text-white">Software Engineer</h1>
+      </div>
+      <div className="text-center flex flex-col items-center justify-center mt-20">
+        <div className={styles.mouse}>
+          <span></span>
+        </div>
+        <div className="text-white text-center mt-4">Scroll Down</div>
       </div>
     </div>
   );
