481c487c06efa7065ccc730178a4ca7d5b5c2d30
Aathif <aathifzahir12@gmail.com>
2024-12-17 11:55:13 +0530

    style(projects)
    styled the project cards

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -12,22 +12,41 @@ type ProjectsProps = {
 
 const Projects = ({ project }: ProjectsProps) => {
   return (
-    <div className="project-card">
-      <h2 className="text-2xl font-bold mb-2">{project.name}</h2>
-      <p className="text-gray-700">{project.description}</p>
-      <p className="text-sm text-gray-500">
-        Technologies: {project.technologies.join(", ")}
-      </p>
-      <p className="text-sm text-gray-500">Date: {project.date}</p>
-      <a
-        href={project.link}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-blue-500 hover:underline"
-      >
-        View Project
-      </a>
-      <img src={project.image} alt={project.name} className="mt-4" />
+    <div className="project-card bg-white p-8 rounded-lg shadow-md m-4 flex justify-between">
+      <div className="w-1/2 space-y-4">
+        <h2 className="text-2xl font-bold mb-2 text-background">
+          {project.name}
+        </h2>
+        <div className="flex flex-wrap gap-2">
+          {project.technologies.map((item: string, index: number) => (
+            <span
+              key={index}
+              className="text-white bg-black rounded-lg px-2 py-1"
+            >
+              {item}
+            </span>
+          ))}
+        </div>
+        <p className="text-gray-700 text-background">{project.description}</p>
+        <p className="text-sm text-gray-500 text-background">
+          Date: {project.date}
+        </p>
+        <div>
+          <a
+            href={project.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500 hover:underline"
+          >
+            View Project
+          </a>
+        </div>
+      </div>
+      <img
+        src={project.image}
+        alt={project.name}
+        className="mt-4 w-1/3 rounded-lg"
+      />
     </div>
   );
 };
