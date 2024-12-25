0703f5302348c4f58a1055f439454bbf5fe93c17
Aathif <aathifzahir12@gmail.com>
2024-12-26 01:38:20 +0530

    style: add borders
    added borders to the background of the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,26 @@ import ProjectData from "./data/projects.json";
 export default function Home() {
   return (
     <div>
+      {/*background border*/}
+      <div
+        className="absolute top-10 left-0 bg-p2"
+        style={{ width: "100%", height: "0.5px" }}
+      ></div>{" "}
+      <div
+        className="absolute bottom-10 left-0 bg-p2"
+        style={{ width: "100%", height: "0.5px" }}
+      ></div>{" "}
+      <div
+        className="absolute top-0 left-10 bg-p2"
+        style={{ width: "0.5px", height: "100%" }}
+      ></div>{" "}
+      <div
+        className="absolute top-0 right-10 bg-p2"
+        style={{ width: "0.5px", height: "100%" }}
+      ></div>{" "}
       <CustomCursor />
+      {/*container*/}
+      <div></div>
     </div>
   );
 }
