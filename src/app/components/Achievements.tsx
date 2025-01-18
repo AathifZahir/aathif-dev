82f7d093551e5d7e60d5066250890dacd6f9d85a
Aathif <aathifzahir12@gmail.com>
2025-01-18 17:51:57 +0530

    chore: correct sizes and spacing for Achievements

--- src/app/components/Achievements.tsx
+++ src/app/components/Achievements.tsx
@@ -1,12 +1,10 @@
 "use client";
 
 import AchievmentList from "../data/achievements.json";
-import { FreeMode } from "swiper/modules";
+import { FreeMode } from "swiper/modules"; // Import Pagination as well
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/free-mode"; // Import for FreeMode
+import "swiper/css/free-mode";
 
 console.log(AchievmentList);
 
@@ -21,29 +19,35 @@ const achievements: Achievement[] = AchievmentList.achievements;
 
 export default function Achievements() {
   return (
-    <div className="py-3 w-10/12 mx-auto">
+    <div className="py-8 w-10/12 mx-auto">
       <div className="container">
-        <h1 className="text-4xl text-center font-bold text-p4">Achievements</h1>
-        <div className="gap-4 mt-5">
+        <h1 className="text-4xl text-center font-bold text-p4 mb-8">
+          Achievements
+        </h1>
+        <div className="mt-5 relative">
+          <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-transparent from-80% to-background z-10 pointer-events-none" />
+
           <Swiper
             slidesPerView={3}
             spaceBetween={30}
             freeMode={true}
             modules={[FreeMode]}
-            className="mySwiper"
+            className="mySwiper relative"
+            mousewheel={true}
+            touchEventsTarget="container"
           >
             {achievements.map((achievement) => (
               <SwiperSlide key={achievement.title}>
-                <div className="bg-background p-4 rounded-lg shadow-lg">
+                <div className="p-8 rounded-lg shadow-lg bg-p6/50 h-[450px] flex flex-col justify-between gap-2 w-96">
                   <img
                     src={achievement.image}
                     alt={achievement.title}
                     className="w-full h-40 object-cover rounded-lg"
                   />
-                  <h2 className="text-2xl font-bold text-p4 mt-2">
+                  <h2 className="text-xl font-bold text-p5 mt-2">
                     {achievement.title}
                   </h2>
-                  <p className="text-p4 mt-2">{achievement.desc}</p>
+                  <p className="text-p5 mt-2">{achievement.desc}</p>
                   <div className="mt-2">
                     {achievement.tags.map((tag) => (
                       <span
